b539833b55e9cedc142e1d59417c01342f699f55
StefanChannel <stefan@channel.surf>
2024-10-04 12:14:54 -0700

    stuff

--- UIResponderKeyboardCache/UIResponder+KeyboardCache.m
+++ UIResponderKeyboardCache/UIResponder+KeyboardCache.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "UIResponder+KeyboardCache.h"
+#import "UIResponderKeyboardCache.h"
 
 static BOOL hasAlreadyCachedKeyboard;
 
